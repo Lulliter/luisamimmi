15e4dc22cf4eda9c2cac78e31047e8faffefd0ec
Lulliter <lmm76@georgetown.edu>
2025-02-15 19:27:05 +0100

    added TEACHING section

--- ___render_deploy.sh
+++ ___render_deploy.sh
@@ -55,7 +55,7 @@ git commit -m "$msg"
 =======
 
 git commit -m "revised colors theme 🎨🖍️"
-git commit -m "added SLOGAN"
+git commit -m "added TEACHING section"
 git commit -m "upd CV"
 		# git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
 
